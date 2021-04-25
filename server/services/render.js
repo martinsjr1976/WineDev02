d890ba2f245ebb3febffc276fc123416655657d7
martinsjr1976 <martinsjr1976@gmail.com>
2021-04-25 20:00:26 +0100

    renderTableSuccess

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@
 const axios=require('axios');
 
 exports.homeRoutes = (req,res)=>{
-    //get request to /api/wines and returning all the data from mongoDB
+    //get request to /api/wines and return all the itens in the collection
     axios.get('http://localhost:3000/api/wines')
         .then(function(response){
             res.render('index',{wines:response.data});
@@ -19,10 +19,5 @@ exports.add_wine = (req,res)=>{
 exports.update_wine = (req,res)=>{
     //passing the id of the selected item to the update page
     axios.get('http://localhost:3000/api/wines', {params:{id:req.query.id}})
-        .then(function(winedata){
-            res.render("update_wine", {wine: winedata.data})
-        })
-    .catch(err=>{
-        res.send(err);
-    })
+    res.render('update_wine');
 }
