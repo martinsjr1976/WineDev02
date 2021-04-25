5f849eef08f274956044dcbbf68614e71228ba6e
martinsjr1976 <martinsjr1976@gmail.com>
2021-04-25 22:19:14 +0100

    deleteDone

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,25 @@ $("#add_wine").submit(function(event){
     alert("Wine inserted successfully!");
 })
 
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function () {
+        var id = $(this).attr("data-id")
+
+        var request = {
+        "url" : `http://localhost:3000/api/wines/${id}`,
+        "method" : "DELETE",
+        }
+
+        if(confirm("Delete the selected wine?")){
+           $.ajax(request).done(function(response){
+               alert("Wine deleted successfully!")
+               location.reload();
+           }) 
+        }
+    })
+}
+
 // $("#update_wine").submit(function(event){
 //     event.preventDefault();
 //     alert("Wine updated successfully!");
