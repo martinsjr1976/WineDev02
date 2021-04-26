d5f4cdbc83d7fcbda829addd49f865553fc037e2
martinsjr1976 <martinsjr1976@gmail.com>
2021-04-26 08:53:21 +0100

    UpdateFormFinal

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,26 +21,28 @@ if(window.location.pathname == "/"){
     })
 }
 
-// $("#update_wine").submit(function(event){
-//     event.preventDefault();
-//     alert("Wine updated successfully!");
+$("#update_wine").submit(function(event){
+    event.preventDefault();
 
-//     var unindexed_array = $(this).serializeArray();
-//     var data = {}
+    var unindexed_array=$(this).serializeArray();
+    var data={}
+    
+    $.map(unindexed_array,function(n,i){
+        data[n['name']]=n['value']
 
-//     $.map(unindexed_array.function (n,i) {
-//         data[n['name']] = n['value']
-//     })
+    })
+    
+    console.log(data);
 
-//     console.log(data);
+    var request={
+        "url":`http://localhost:3000/api/wines/${data.id}`,
+        "method":"PUT",
+        "data":data
+    }
 
-//     var request = {
-//         "url" : `http://localhost:3000/api/wines/${data.id}`,
-//         "method" : "PUT",
-//         "data" : data
-//     }
+    $.ajax(request).done(function(response) {
+        alert("Wine updated successfully!")
+    })
 
-//     $.ajax(request).done(function(response){
-//         alert("Wine updated successfully")
-//     })
-// })
+})
+
